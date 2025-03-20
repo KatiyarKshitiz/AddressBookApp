c0c6a172107dd3a6f9d39d7eb569732c4ee9392a
Kshitiz Katiyar <katiyarkshitiz0912@gmail.com>
2025-03-20 11:15:28 +0530

    Implemented Junit_Testing_and_Redis

--- src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
+++ src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
@@ -2,12 +2,18 @@ package com.example.AddressBookApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
+@EnableCaching
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address-Book App Started in {} Environment",
+				context.getEnvironment().getProperty("spring.profiles.active"));
 	}
-
 }
